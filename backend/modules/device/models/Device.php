4dcd7115f86431764710282424e48492ac54896d
ameur hamdouni <ameur.hamdouni@gmail.com>
2015-08-16 16:45:35 +0100

    add new fields extra_fields to Device and Product Models

--- backend/modules/device/models/Device.php
+++ backend/modules/device/models/Device.php
@@ -2,6 +2,7 @@
 
 namespace backend\modules\device\models;
 
+use backend\behaviors\HasParamsBehavior;
 use backend\modules\brand\models\Brand;
 use backend\modules\category\models\Category;
 use backend\modules\content\models\Content;
@@ -23,6 +24,7 @@ use Yii;
  * @property string $height
  * @property string $depth
  * @property string $price
+ * @property string $extra_fields
  * @property string $note
  * @property string $characters
  * @property Brand $brand
@@ -49,7 +51,7 @@ class Device extends \backend\db\Model
         return [
             [['device_brand_id', 'device_category_id', 'device_product_id'], 'integer'],
             [['type'], 'required'],
-            [['name', 'description', 'short_description', 'characters'], 'string'],
+            [['name', 'description', 'short_description', 'characters', 'extra_fields'], 'string'],
             [['weight', 'width', 'height', 'depth', 'price'], 'number'],
             [['type', 'note'], 'string', 'max' => 25]
         ];
@@ -72,6 +74,7 @@ class Device extends \backend\db\Model
             'weight' => 'Weight',
             'width' => 'Width',
             'height' => 'Height',
+            'extra_fields' => 'Extra Fields',
             'depth' => 'Depth',
             'price' => 'Price',
             'note' => 'Note',
@@ -79,6 +82,19 @@ class Device extends \backend\db\Model
         ];
     }
 
+
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'HasParamsBehavior' => [
+                    'class' => HasParamsBehavior::className(),
+                    'attribute' => 'extra_fields'
+                ],
+            ]
+        );
+    }
 
     /**
      * @return \yii\db\ActiveQuery
@@ -120,6 +136,25 @@ class Device extends \backend\db\Model
     public function getContents()
     {
         return $this->hasMany(Content::className(), ['device_id' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtraFields()
+    {
+
+        return $this->extra_fields;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setExtraFields(array $extra_fields)
+    {
+        $this->extra_fields = $extra_fields;
+
+        return $this;
     }
 
     public function defaultExpand()
