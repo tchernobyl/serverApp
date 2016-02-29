be36a4ffc3d7b517f446144ebe30fbde23bf018c
ameur hamdouni <ameur.hamdouni@gmail.com>
2016-02-29 14:28:35 +0100

    add params

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,24 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=APEDevices',
+            'username' => '',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'settings' => [
+            'class' => 'pheme\settings\components\Settings',
+            'cache' => null
+        ],
     ],
 ];
