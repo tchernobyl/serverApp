7646539f9b1bc9f434377fd087cb97ada6a7603b
tchernobyl <ameur.hamdouni@gmail.com>
2017-04-24 01:57:48 +0100

    quick fix

---
+++ common/config/params.template.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=MDWProject',
+    'username' => 'root',
+    'password' => 'devpass',
+    'charset' => 'utf8',
+];
