c5d18b8beaebdd9c2ee50bc5d040074a1b188851
tchernobyl <ameur.hamdouni@gmail.com>
2017-04-24 01:43:06 +0100

    quick fix

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,9 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=APEDevices',
+    'username' => 'root',
+    'password' => 'devpass',
+    'charset' => 'utf8',
 ];
